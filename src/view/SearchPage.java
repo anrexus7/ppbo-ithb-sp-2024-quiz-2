de96c076e308206b718386d369266994d3155da0
Hans <ishigoemon878@gmail.com>
2024-07-04 21:10:17 +0700

    final update ToT

--- src/view/SearchPage.java
+++ src/view/SearchPage.java
@@ -10,11 +10,11 @@ public class SearchPage extends JFrame {
 
     public void innit() {
         String search = JOptionPane.showInputDialog(null, "Masukkan nomor anggota ");
-        int searching = Integer.parseInt(search);
 
         if(search == null){
             new HomePage();
         }else{
+            int searching = Integer.parseInt(search);
             new ViewPage(searching);
         }
     }
